648893d93fd495b0026f0b457c655b5e798975b2
soniravindra <ravindrasoni@gmail.com>
2023-10-19 18:20:50 +0530

    Devashish

--- Modules/LandingPage/Entities/LandingPageSetting.php
+++ Modules/LandingPage/Entities/LandingPageSetting.php
@@ -29,7 +29,7 @@ class LandingPageSetting extends Model
 
         // $settings = [
         //     "topbar_status" => "on",
-        //     "topbar_notification_msg" => "Welcome To Kaiten Software",
+        //     "topbar_notification_msg" => "Devashish",
 
         //     "menubar_status" => "on",
         //     "menubar_page"=>'',
